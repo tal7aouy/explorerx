6f731ec2b05a3f35f55e41751603e6527857f84e
tal7aouy <tal7aouy.dev@gmail.com>
2023-11-18 19:13:27 +0100

    add validated request

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\storeFolderRequest;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -11,4 +12,14 @@ class FileController extends Controller
     {
         return Inertia::render('Admin/Files/Index');
     }
+
+    public function createFolder(storeFolderRequest $request)
+    {
+        $data = $request->validated();
+        $parent = $request->parent;
+        if (!$parent){
+            $parent = $this->getRoot();
+        }
+    }
+
 }
---
+++ app/Http/Requests/storeFolderRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class storeFolderRequest extends parentBaseIdRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return array_merge(parent::rules(),[
+          'name'=> 'required',
+          Rule::unique(File::class,'name')
+            ->where('created_by', Auth::user()->id)
+            ->where('parent_id',  $this->parent_id)
+          ->whereNull('deleted_at')
+        ]);
+    }
+
+  public function messages()
+  {
+    return [
+      'name.unique'=> "Folder ':input' already exist"
+    ];
+    }
+}
